ce64f37e4081ffae3a22b09527d83aa464f8c36a
Softt2 <colt.b.cruz@gmail.com>
2024-10-03 17:50:33 -0400

    EnemyAI Updated

--- Assets/Scripts/EnemyAI/enemyAI.cs
+++ Assets/Scripts/EnemyAI/enemyAI.cs
@@ -6,8 +6,8 @@ using UnityEngine.AI;
 public class enemyAI : Unit
 {
     [Header("Render Components")]
-    [SerializeField] Animator animator; 
-    [SerializeField] Renderer model; 
+    [SerializeField] Animator animator;
+    [SerializeField] Renderer model;
     [SerializeField] ParticleSystem particle;
     //[SerializeField] TextMeshPro healthText; 
     [SerializeField] CapsuleCollider bodyCollider;
@@ -24,6 +24,14 @@ public class enemyAI : Unit
     [SerializeField] GameObject bolt; 
     [Range(0, 3)][SerializeField] float attackRate;
 
+    [Header("Aggro and Roaming")]
+    [Range(0, 25)][SerializeField] float aggroRange;
+    [Range(0, 25)][SerializeField] float roamRange;
+    [Range(50, 150)][SerializeField] float renderDistance;
+    [SerializeField] int viewAngle; 
+
+    Vector3 playerDir;
+
     Color colorOrig;
 
     bool playerInRange;
@@ -32,7 +40,10 @@ public class enemyAI : Unit
     public bool isMoving;
     public bool canAttack = true;
 
-    Vector3 playerDir;
+    float angleToPLayer;
+    float stopDistOrig;
+
+    Coroutine someCo;  
 
     // Start is called before the first frame update
     void Start()
@@ -43,7 +54,47 @@ public class enemyAI : Unit
     // Update is called once per frame
     void Update()
     {
-       
+        // Check if player is in range
+        if (IsPlayerInRange())
+        {
+            // Move towards the player
+            agent.Move(playerDir);
+
+            // Face the player
+            FaceTarget();
+
+            // Shoot at the player
+            if (!isAttacking)
+            {
+                //StartCoroutine(shoot());
+            }
+        }
+        else
+        {
+            // Stop moving and shooting
+            agent.Move(Vector3.zero);
+            StopAllCoroutines();
+        }
+    }
+
+    private bool IsPlayerInRange()
+    {
+        // Raycast to check if the player is within aggro range
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPLayer = Vector3.Angle(playerDir, transform.forward); 
+
+        Debug.DrawRay(headPos.position, playerDir);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPLayer <= viewAngle)   
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void OnTriggerEnter(Collider other)
