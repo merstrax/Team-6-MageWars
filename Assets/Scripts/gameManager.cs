2cd30075ecc60d929bd9f1c2a6d2a018592a26ff
Softt2 <colt.b.cruz@gmail.com>
2024-10-03 17:20:17 -0400

    GameManager

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -4,15 +4,95 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] TMP_Text enemyCountText;
+
+    public GameObject playerSpawnPos;
+    public TMP_Text ammoCurrent, ammoMax;
+    public Image PlayerHPBar;
+
+    public GameObject checkPointPopUp;
+    public GameObject damagePanel;
+    public GameObject player;
+
+    public playerController playerScript;
+
+    int enemyCount;
+    float timeScaleOrig;
+
+    public bool isPaused;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
+        timeScaleOrig = Time.timeScale;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        playerSpawnPos = GameObject.FindGameObjectWithTag("Player Spawn Pos");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
     }
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+
+
+    public void updateGameGoal(int amount)
+    {
+        enemyCount += amount;
+        enemyCountText.text = enemyCount.ToString("F0");
+
+        if (enemyCount <= 0)
+        {
+            // player wins the game
+            //Debug.Log("You Win!!"); 
+            statePause();
+            menuActive = menuWin;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
 }
