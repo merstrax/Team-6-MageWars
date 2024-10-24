29a1beec20c75ab79b6400fce9c0d83aa55049dc
Michael Amundson <merstrax@gmail.com>
2024-10-24 12:22:26 -0500

    Audio Screen functionality

--- Assets/Scripts/User Interface/Sound.cs
+++ Assets/Scripts/User Interface/Sound.cs
@@ -4,70 +4,74 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class Sound : MonoBehaviour
 {
-    public AudioMixer audioMixer;
+    [Header("Audio")]
+    [SerializeField] Slider volumeMaster;
+    [SerializeField] Slider volumeMusic;
+    [SerializeField] Slider volumeEffects;
 
-    public TMP_Text mastLabel, musicLabel, sfxLabel;
+    [SerializeField] AudioMixer volumeMixer;
 
-    public Slider mastSlider, musicSlider, sfxSlider;
-
-    // Start is called before the first frame update
     void Start()
     {
+        float _vol = PlayerPrefs.GetFloat("volumeMaster", 0.0f);
+        volumeMaster.value = PlayerPrefs.GetFloat("volumeMaster", 0.75f);
+        UpdateSlider(volumeMaster);
+
+        _vol = PlayerPrefs.GetFloat("volumeEffects", 0.0f);
+        volumeEffects.value = PlayerPrefs.GetFloat("volumeEffects", 0.75f);
+        UpdateSlider(volumeEffects);
+
+        _vol = PlayerPrefs.GetFloat("volumeBG", 0.0f);
+        volumeMusic.value = PlayerPrefs.GetFloat("volumeBG", 0.75f);
+        UpdateSlider(volumeMusic);
+
         LoadSettings();
-        Apply();
+
+        gameObject.SetActive(false);
     }
 
-    public void SetMasterVol()
+    void LoadSettings()
     {
-        float _val = 100 * ((mastSlider.value + 80) / 80);
-
-        mastLabel.text = _val.ToString("0");
-    }
-    public void SetMusicVol()
-    {
-        float _val = 100 * ((musicSlider.value + 80) / 80);
-
-        musicLabel.text = _val.ToString("0");
-    }
-    public void SetSFXVol()
-    {
-        float _val = 100 * ((sfxSlider.value + 80) / 80);
-
-        sfxLabel.text = _val.ToString("0");
+        ChangeVolume();
     }
 
-    public void LoadSettings()
-    {
-        mastSlider.value = PlayerPrefs.GetFloat("MasterVolumePreference", 1.0f);
-        musicSlider.value = PlayerPrefs.GetFloat("MusicVolumePreference", 1.0f);
-        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1.0f);
-    }
     public void Accept()
     {
-        Apply();
+        ChangeVolume();
         GameManager.instance.ReturnToPrev();
     }
+
     public void Back()
     {
         LoadSettings();
         GameManager.instance.ReturnToPrev();
     }
-    private void Apply()
+
+    public void ChangeVolume()
     {
-        float _val = (80 * (mastSlider.value / 100)) - 80;
-        audioMixer.SetFloat("MasterVol", _val);
-        PlayerPrefs.SetFloat("MasterVolumePreference", _val);
+        volumeMixer.SetFloat("volumeMaster", Mathf.Log10(volumeMaster.value) * 20);
+        PlayerPrefs.SetFloat("volumeMaster", volumeMaster.value);
 
-        _val = (80 * (musicSlider.value / 100)) - 80;
-        audioMixer.SetFloat("MusicVol", _val);
-        PlayerPrefs.SetFloat("MusicVolumePreference", _val);
+        volumeMixer.SetFloat("volumeEffects", Mathf.Log10(volumeEffects.value) * 20);
+        PlayerPrefs.SetFloat("volumeEffects", volumeEffects.value);
 
-        _val = (80 * (sfxSlider.value / 100)) - 80;
-        audioMixer.SetFloat("SFXVol", _val);
-        PlayerPrefs.SetFloat("SFXVolumePreference", _val);
+        volumeMixer.SetFloat("volumeBG", Mathf.Log10(volumeMusic.value) * 20);
+        PlayerPrefs.SetFloat("volumeBG", volumeMusic.value);
+    }
 
+    public void UpdateSlider(Slider slider)
+    {
+        slider.GetComponentInChildren<TMP_Text>().text = (slider.value * 100.0f).ToString("N0");
+    }
+
+    public void UpdateInput(Slider slider)
+    {
+        string _value = slider.GetComponentInChildren<TMP_Text>().text;
+        if (_value.Length > 0)
+            slider.value = float.Parse(_value);
     }
 }
